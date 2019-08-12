45bdc493c92df8730216aacd2764f51128606189
ZeroHe <897457487@qq.com>
2019-08-12 10:14:46 +0800

    新增MySQL.newGKAdder()方法

--- src/main/java/hwp/sqlte/MySQL.java
+++ src/main/java/hwp/sqlte/MySQL.java
@@ -8,6 +8,11 @@
 
 package hwp.sqlte;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.concurrent.atomic.LongAdder;
+import java.util.function.BiConsumer;
 import java.util.function.Function;
 
 /**
@@ -19,4 +24,18 @@ public class MySQL {
     public static final Function<String, String> TO_INSERT_IGNORE_INTO =
             sql -> sql.replace("INSERT INTO", "INSERT IGNORE INTO");
 
+    public static BiConsumer<PreparedStatement, int[]> newGKAdder(LongAdder adder) {
+        return (ps, ints) -> {
+            try {
+                ResultSet keys = ps.getGeneratedKeys();//MySQL只有自增ID才会返回
+                if (keys != null) {
+                    if (keys.last()) {
+                        adder.add(keys.getRow());
+                    }
+                }
+            } catch (SQLException e) {//如果不支持滚动会报异常
+                throw new UncheckedException(e);
+            }
+        };
+    }
 }
